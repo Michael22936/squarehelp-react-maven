ebcc123b0336834b30678da603b2ed77cb3d5b09
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-08-05 20:42:45 -0500

    boiler code

---
+++ java/com/squarehelpwithreact/reactjsmaven/ReactjsMavenApplication.java
@@ -0,0 +1,13 @@
+package com.squarehelpwithreact.reactjsmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactjsMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactjsMavenApplication.class, args);
+    }
+
+}
